dc05cf29f2dd640f5f9fe2e5386c320e0b89e788
watashiwaaniket <aniketdhakane3@gmail.com>
2026-07-03 12:16:41 +0530

    starting out the project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <section className="w-screen h-screen flex justify-center bg-[repeating-linear-gradient(45deg,#6FBEB2_0,#6FBEB2_1px,#0B0909_0,#0B0909_10px)]">
+      <div className="w-xl sm:w-2xl md:w-6xl min-h-screen bg-[#0B0909] border-x border-[#6FBEB2] text-[#A5E9DD] p-8">
+        <h2 className="text-xl sm:text-2xl md:text-3xl lg:text-6xl">
+          Typescript Lessons
+        </h2>
+      </div>
+    </section>
+  );
+}
+
+export default App;
